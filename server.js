0c1c94ae3b4b1d34fc99a314fb30a034649d6dba
Varun Srivastava <varunsrivastava1904@gmail.com>
2018-02-27 15:18:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,29 +38,33 @@ function createTemplate(data){
     var date=data.date;
     var headng=data.heading;
     var content=data.content;
-var htmlTemplate=`<html>
+var htmlTemplate=`
+<html>
     <head>
-        <title> Article 1 Varun Srivastava</title>
+        <title>
+            ${title}
+        </title>
         <meta name="viewport" content="width=device-width, initial scale=1"/>
-       <link href="/ui/style.css" rel="stylesheet" />
-        </style>
+        <link href="/ui/style.css" rel="stylesheet" />
     </head>
     <body>
-        <div class = "Container">
+        <div class = "container">
             <div>
-        <a href ="/">Home</a>    
-        </div>    
-        <hr/>
-        <h3>${heading}</h3>
-        <div> ${date}
-        <div> 
-       ${content}
+                <a href ="/">Home</a>    
+            </div>    
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div> 
+            <div>
+                ${content}
+            </div>
         </div>
-        </div>
-        
     </body>
 </html>
-
 `;
 return htmlTempate;
 }
