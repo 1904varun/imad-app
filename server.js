13e9431f0a361baace165528097008ee273c6141
Varun Srivastava <varunsrivastava1904@gmail.com>
2018-03-04 23:05:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,89 +5,23 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles={
-'article-one' :{
-title: "Article 1 | Varun Srivastava",
-heading:"Article 1",
-date:"Feb 25 2018",
-content:`<p>This is the content of my first article ofthe first web page</p>
-<p>This is the content of my first article ofthe first web page
-    </p>
-     <p>This is the content of my first article ofthe first web page
-    </p>
-     <p>This is the content of my first article ofthe first web page
-    </p>`
-},
-'article-two' :{
-title: "Article 2 | Varun Srivastava",
-heading:"Article 2",
-date:"Feb 27 2018",
-content:`<p>jjhdsjhjhfdjhjzdfzdfzdfzzfz</p>`
-
-},
-'article-three' :{
-title: "Article 3 | Varun Srivastava",
-heading:"Article 3",
-date:"Feb 27 2018",
-content:`<p>lfkajhdajfhjdahfjbfjbjba3333333333</p>`
-}
-};
-
-function createTemplate(data){
-var title=data.title;
-var date=data.date;
-var headng=data.heading;
-var content=data.content;
-var htmlTemplate=`
-<html>
-<head>
-    <title>
-        ${title}
-    </title>
-    <meta name="viewport" content="width=device-width, initial scale=1"/>
-    <link href="/ui/style.css" rel="stylesheet" />
-</head>
-<body>
-    <div class = "container">
-        <div>
-            <a href ="/">Home</a>    
-        </div>    
-        <hr/>
-        <h3>
-            ${heading}
-        </h3>
-        <div>
-            ${date}
-        </div> 
-        <div>
-            ${content}
-        </div>
-    </div>
-</body>
-</html>
-`;
-return htmlTempate;
-}
 app.get('/', function (req, res) {
-res.send(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function(req, res) {
-//articleName== article-one
-var articleName = req.params.articleName;
-res.send(createTemplate(articles[articleName])); 
-});
 app.get('/ui/style.css', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
+
 var port = 80;
 app.listen(port, function () {
-console.log(`IMAD course app listening on port ${port}!`);
+  console.log(`IMAD course app listening on port ${port}!`);
 });
